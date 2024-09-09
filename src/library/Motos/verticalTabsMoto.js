eb58877128b186e88a47c11c1123b9b8031d3eb6
‘Alex’ <oleksii.bashinskii@mirs.com>
2024-09-09 11:17:58 +0300

    last changes

--- src/library/Motos/verticalTabsMoto.js
+++ src/library/Motos/verticalTabsMoto.js
@@ -23,18 +23,14 @@ const VerticalTabs = (props) => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    // Функция-обработчик для обновления ширины окна
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
 
-    // Добавляем обработчик события изменения размера окна
     window.addEventListener("resize", handleResize);
 
-    // Устанавливаем начальное значение ширины окна
     setWindowWidth(window.innerWidth);
 
-    // Удаляем обработчик события при размонтировании компонента
     return () => {
       window.removeEventListener("resize", handleResize);
     };
